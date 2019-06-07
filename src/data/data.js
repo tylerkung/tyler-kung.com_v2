89370774ada1f4e8da187aea819a45957763da51
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-06 17:13:51 -0700

    more base files

---
+++ src/data/data.js
@@ -0,0 +1,67 @@
+export default{
+    "data": [
+    	  {
+    		  "title": "Bow Wow",
+    		  "image-1": "/img/events/bowwow-origin.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Friends at the Function",
+    		  "image-1": "/img/events/friends-theknockout.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Noodles",
+    		  "image-1": "/img/events/noodles-1015.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Function",
+    		  "image-1": "/img/events/function-mezzanine.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Friends at the Function",
+    		  "image-1": "/img/events/function-theknockout.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Wootoberfest",
+    		  "image-1": "/img/events/wootoberfest-theendup.png",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Friend Request",
+    		  "image-1": "/img/events/friendrequest-origin.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Don't Miss Ya Friends",
+    		  "image-1": "/img/events/dontmiss-rustedmule.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  },
+    	  {
+    		  "title": "Space 2950",
+    		  "image-1": "/img/events/space2950-f8.jpg",
+    		  "image-2": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    		  "link": ""
+    	  }
+      ]
+}
