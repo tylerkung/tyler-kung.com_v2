8aebd5b09db96b3d4d7d65837718b27ea37ed21b
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-10 13:39:23 -0700

    swup

--- dist/scripts/all.js
+++ dist/scripts/all.js
@@ -1,1 +1,4 @@
 "use strict";
+
+// import Swup from 'swup';
+var swup = new Swup(); // only this line when included with script tag
---
+++ src/assets/scripts/scripts.js
@@ -0,0 +1,2 @@
+// import Swup from 'swup';
+var swup = new Swup(); // only this line when included with script tag
