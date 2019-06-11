355556a5546065e39287df19f6e63be26490a885
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-10 17:04:59 -0700

    swup and nav

--- dist/scripts/all.js
+++ dist/scripts/all.js
@@ -1,4 +1,22 @@
-"use strict";
+'use strict';
 
 // import Swup from 'swup';
 var swup = new Swup(); // only this line when included with script tag
+
+var menuApp = new Vue({
+    el: '.nav',
+    data: {
+        menuOpen: false
+    }
+});
+
+swup.on('animationOutStart', function () {
+    menuApp.menuOpen = false;
+    // location.reload(true);
+});
+
+// $(document).ready(function(){
+// $('.menu-button').click(function(){
+//     $('.menu, .menu-overlay').toggleClass('active');
+// })
+// })
--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -1,2 +1,20 @@
 // import Swup from 'swup';
 var swup = new Swup(); // only this line when included with script tag
+
+var menuApp = new Vue({
+    el: '.nav',
+    data: {
+        menuOpen: false
+    },
+})
+
+swup.on('animationOutStart', function(){
+    menuApp.menuOpen = false;
+    // location.reload(true);
+});
+
+// $(document).ready(function(){
+    // $('.menu-button').click(function(){
+    //     $('.menu, .menu-overlay').toggleClass('active');
+    // })
+// })
