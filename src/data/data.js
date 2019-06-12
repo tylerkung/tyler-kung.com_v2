0e5c014d7e17333b88adab6b3cacd7f56f12c70b
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-12 16:50:48 -0700

    homepage project list

--- src/data/data.js
+++ src/data/data.js
@@ -1,67 +1,69 @@
 export default{
     "data": [
     	  {
-    		  "title": "Bow Wow",
-    		  "image-1": "/img/events/bowwow-origin.jpg",
+    		  "title": "Basketball Monster",
+    		  "image-1": "/img/basketballmonster.jpg",
     		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "headline": "The problem with winning",
+    		  "description": "I redesigned a popular web tool for fantasy basketball and wrote about my findings.",
+              "tags": "UI/UX, User Research, Design",
+              "date": "2018",
+    		  "link": "",
+              "link-name": ""
     	  },
     	  {
     		  "title": "Friends at the Function",
-    		  "image-1": "/img/events/friends-theknockout.jpg",
+    		  "image-1": "/img/metabolon.jpg",
     		  "image-2": "",
+              "headline": "",
     		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "tags": "UI/UX, User Research, Design",
+    		  "link": "",
+              "link-name": ""
     	  },
     	  {
     		  "title": "Noodles",
-    		  "image-1": "/img/events/noodles-1015.jpg",
+    		  "image-1": "/img/Vineti.jpg",
     		  "image-2": "",
+              "headline": "",
     		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "tags": "UI/UX, User Research, Design",
+              "date": "",
+    		  "link": "",
+              "link-name": ""
     	  },
-    	  {
-    		  "title": "Function",
-    		  "image-1": "/img/events/function-mezzanine.jpg",
+          {
+    		  "title": "Basketball Monster",
+    		  "image-1": "/img/basketballmonster.jpg",
     		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "headline": "The problem with winning",
+    		  "description": "I did a redesign on a popular web tool for fantasy basketball and wrote about my findings.",
+              "tags": "",
+              "date": "",
+    		  "link": "",
+              "link-name": ""
     	  },
     	  {
     		  "title": "Friends at the Function",
-    		  "image-1": "/img/events/function-theknockout.jpg",
+    		  "image-1": "/img/metabolon.jpg",
     		  "image-2": "",
+              "headline": "",
     		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "tags": "",
+              "date": "",
+    		  "link": "",
+              "link-name": ""
     	  },
     	  {
-    		  "title": "Wootoberfest",
-    		  "image-1": "/img/events/wootoberfest-theendup.png",
+    		  "title": "Noodles",
+    		  "image-1": "/img/Vineti.jpg",
     		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
-    	  },
-    	  {
-    		  "title": "Friend Request",
-    		  "image-1": "/img/events/friendrequest-origin.jpg",
-    		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
-    	  },
-    	  {
-    		  "title": "Don't Miss Ya Friends",
-    		  "image-1": "/img/events/dontmiss-rustedmule.jpg",
-    		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
-    	  },
-    	  {
-    		  "title": "Space 2950",
-    		  "image-1": "/img/events/space2950-f8.jpg",
-    		  "image-2": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    		  "link": ""
+              "headline": "",
+    		  "description": "Excepteur sint occaecat cupidatat non proident, suntin culpa qui officia deserunt mollit anim id est laborum.",
+              "tags": "",
+              "date": "",
+    		  "link": "",
+              "link-name": ""
     	  }
       ]
 }
