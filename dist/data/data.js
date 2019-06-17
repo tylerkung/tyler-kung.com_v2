c188337efc3f0d41a4f2d7d3f6ea6acd45c1fa02
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-17 16:40:05 -0700

    About page, work pages

--- dist/data/data.js
+++ dist/data/data.js
@@ -1,16 +1,28 @@
 export default{
     "data": [
+
     	  {
-    		  "title": "Basketball Monster",
-    		  "image-1": "/img/basketballmonster.jpg",
+    		  "title": "Friends",
+    		  "image-1": "/img/friends.png",
     		  "image-2": "",
-              "headline": "The problem with winning",
-    		  "description": "I redesigned a popular web tool for fantasy basketball and wrote about my findings.",
-              "tags": "UI/UX, User Research, Design",
-              "date": "2018",
-    		  "link": "/",
+              "headline": "A platform for music loves and creative minds",
+    		  "description": "I teamed up with close friend Mike Woo, creator of FRIENDS, to bring his brand to a digital space and to design, develop, execute their website.",
+              "tags": "Strategy, Design, Development",
+              "date": "2019",
+    		  "link": "/work/friends",
               "link-name": "View Case Study"
     	  },
+          {
+     		  "title": "Basketball Monster",
+     		  "image-1": "/img/basketballmonster.jpg",
+     		  "image-2": "",
+               "headline": "The problem with winning",
+     		  "description": "I redesigned a popular web tool for fantasy basketball and wrote about my findings.",
+               "tags": "UI/UX, User Research, Design",
+               "date": "2018",
+     		  "link": "/",
+               "link-name": "View Case Study"
+     	  },
           {
     		  "title": "Vineti",
     		  "image-1": "/img/Vineti.jpg",
@@ -29,6 +41,7 @@ export default{
               "headline": "",
     		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
               "tags": "UI/UX, User Research, Design",
+              "date": "2018",
     		  "link": "/",
               "link-name": "Read More"
     	  },
@@ -39,7 +52,7 @@ export default{
               "headline": "The problem with winning",
     		  "description": "I did a redesign on a popular web tool for fantasy basketball and wrote about my findings.",
               "tags": "",
-              "date": "",
+              "date": "2018",
     		  "link": "/",
               "link-name": "Read More"
     	  },
@@ -50,7 +63,7 @@ export default{
               "headline": "",
     		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
               "tags": "",
-              "date": "",
+              "date": "2018",
     		  "link": "/",
               "link-name": "Read More"
     	  },
