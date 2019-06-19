47f72aa0fafee334ef8b652504f8ba6614652d73
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-18 17:03:48 -0700

    TAC page

--- dist/data/data.js
+++ dist/data/data.js
@@ -1,27 +1,37 @@
 export default{
     "data": [
-
     	  {
     		  "title": "Friends",
     		  "image-1": "/img/friends.png",
     		  "image-2": "",
-              "headline": "A platform for music loves and creative minds",
-    		  "description": "I teamed up with close friend Mike Woo, creator of FRIENDS, to bring his brand to a digital space and to design, develop, execute their website.",
+              "headline": "A platform for music lovers and creative minds",
+    		  "description": "I teamed up with close friend Mike Woo, creator of FRIENDS, to bring his brand to a digital space and to design, develop, and execute their website.",
               "tags": "Strategy, Design, Development",
               "date": "2019",
     		  "link": "/work/friends",
               "link-name": "View Case Study"
     	  },
           {
+     		  "title": "The Animal Connection",
+     		  "image-1": "/img/ac.jpg",
+     		  "image-2": "",
+               "headline": "San Francisco's largest, independently owned pet store",
+     		  "description": "My design process, brainstorming, researching, and execution for The Animal Connection's stickers.",
+               "tags": "Branding, Logo, Stickers",
+               "date": "2019",
+     		  "link": "/work/the-animal-connection",
+               "link-name": "View Case Study"
+     	  },
+          {
      		  "title": "Basketball Monster",
      		  "image-1": "/img/basketballmonster.jpg",
      		  "image-2": "",
                "headline": "The problem with winning",
-     		  "description": "I redesigned a popular web tool for fantasy basketball and wrote about my findings.",
+     		  "description": "I redesigned a popular web tool for fantasy basketball and wrote about my findings which was published on UX Collective on Medium.",
                "tags": "UI/UX, User Research, Design",
                "date": "2018",
-     		  "link": "/",
-               "link-name": "View Case Study"
+     		  "link": "https://uxdesign.cc/the-problem-with-winning-redesigning-a-web-app-that-helps-me-beat-my-friends-a7b7af9195cb",
+               "link-name": "Read Article"
      	  },
           {
     		  "title": "Vineti",
@@ -31,52 +41,8 @@ export default{
     		  "description": "I was the lead front-end developer at HDMZ for Vineti's corporate website, who provide software for personalized therapies.",
               "tags": "Development, Front-End, SVG Animation",
               "date": "2018",
-    		  "link": "/",
-              "link-name": "Read More"
-    	  },
-    	  {
-    		  "title": "Friends at the Function",
-    		  "image-1": "/img/metabolon.jpg",
-    		  "image-2": "",
-              "headline": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              "tags": "UI/UX, User Research, Design",
-              "date": "2018",
-    		  "link": "/",
-              "link-name": "Read More"
-    	  },
-          {
-    		  "title": "Basketball Monster",
-    		  "image-1": "/img/basketballmonster.jpg",
-    		  "image-2": "",
-              "headline": "The problem with winning",
-    		  "description": "I did a redesign on a popular web tool for fantasy basketball and wrote about my findings.",
-              "tags": "",
-              "date": "2018",
-    		  "link": "/",
-              "link-name": "Read More"
-    	  },
-    	  {
-    		  "title": "Friends at the Function",
-    		  "image-1": "/img/metabolon.jpg",
-    		  "image-2": "",
-              "headline": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              "tags": "",
-              "date": "2018",
-    		  "link": "/",
-              "link-name": "Read More"
-    	  },
-    	  {
-    		  "title": "Noodles",
-    		  "image-1": "/img/Vineti.jpg",
-    		  "image-2": "",
-              "headline": "",
-    		  "description": "Excepteur sint occaecat cupidatat non proident, suntin culpa qui officia deserunt mollit anim id est laborum.",
-              "tags": "",
-              "date": "",
-    		  "link": "/",
-              "link-name": "Read More"
+    		  "link": "https://vineti.com/",
+              "link-name": "View Website"
     	  }
       ]
 }
