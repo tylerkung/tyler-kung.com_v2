b14ced7b53d3f37f827d03ad944219b1403283d8
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-07-09 12:10:51 -0700

    home animation

--- dist/scripts/all.js
+++ dist/scripts/all.js
@@ -11,9 +11,18 @@ $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
     $('.list .list-item').each(function () {
         new ScrollMagic.Scene({
-            triggerElement: this
+            triggerElement: this,
+            triggerHook: 0.6
         }).setClassToggle(this, 'reveal')
         // .addIndicators()
         .reverse(false).addTo(controller);
     });
+
+    new ScrollMagic.Scene({
+        triggerElement: '.dark-header .main-header',
+        triggerHook: 0.03,
+        duration: $('.dark-header .main-header').innerHeight()
+    }).setClassToggle('.menu', 'dark')
+    // .addIndicators()
+    .addTo(controller);
 });
